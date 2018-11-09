cd9f8c5f264480f6438a0664adfc657adeeaa810
Niry Andriamanga <andriama@in.tum.de>
2018-11-09 10:32:09 +0100

    Added different scope to allow different RNN architectures

--- mrf_recon_rnn_par_search.py
+++ mrf_recon_rnn_par_search.py
@@ -65,68 +65,69 @@ relaxation_times /= times_max
 
 from rnn_functions import RNN
 
-for lr in learning_rate:
-    for nh in num_hidden:
-        logits = RNN(X, timesteps, nh, num_output)
+for nh in num_hidden:
+    for lr in learning_rate:
+        with tf.variable_scope("nh{}".format(nh)):
+            logits = RNN(X, timesteps, nh, num_output)
 
 # Define loss and optimizer
-        loss_op = tf.losses.mean_squared_error(Y, logits)
-        #loss_op = tf.reduce_mean(tf.abs(tf.divide(tf.subtract(Y, logits), Y))) # mean averaged percentage error
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
-        train_op = optimizer.minimize(loss_op)
+            loss_op = tf.losses.mean_squared_error(Y, logits)
+            #loss_op = tf.reduce_mean(tf.abs(tf.divide(tf.subtract(Y, logits), Y))) # mean averaged percentage error
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+            train_op = optimizer.minimize(loss_op)
 
 # Evaluate model (with test logits, for dropout to be disabled)
-        mse_t1 = tf.losses.mean_squared_error(labels=times_max[0]*Y[:, 0], predictions=times_max[0]*logits[:, 0])
-        mse_t2 = tf.losses.mean_squared_error(labels=times_max[1]*Y[:, 1], predictions=times_max[1]*logits[:, 1])
-        out = times_max * logits
+            mse_t1 = tf.losses.mean_squared_error(labels=times_max[0]*Y[:, 0], predictions=times_max[0]*logits[:, 0])
+            mse_t2 = tf.losses.mean_squared_error(labels=times_max[1]*Y[:, 1], predictions=times_max[1]*logits[:, 1])
+            out = times_max * logits
 
 # Initialize the variables (i.e. assign their default value)
-        init = tf.global_variables_initializer()
+            init = tf.global_variables_initializer()
 
 # Summaries to view in tensorboard
-        train_loss_summary = tf.summary.scalar('training_loss', loss_op)
-        val_loss_summary = tf.summary.scalar('validation_loss', loss_op)
+            train_loss_summary = tf.summary.scalar('training_loss', loss_op)
+            val_loss_summary = tf.summary.scalar('validation_loss', loss_op)
 #merged = tf.summary.merge_all()
 
 # Saver
-        saver = tf.train.Saver()
+            saver = tf.train.Saver()
 
 # Restoration directory
-        ckpt_dir = 'rnn_model/'
+            ckpt_dir = 'rnn_model/'
 
 # Start training
-        with tf.Session() as sess:
+            with tf.Session() as sess:
 
     # Run the initializer
-            sess.run(init)
+                sess.run(init)
         
-            train_loss_writer = tf.summary.FileWriter('tensorboard/training_loss_lr{}_nh{}/'.format(lr, nh), sess.graph)
-            val_loss_writer = tf.summary.FileWriter('tensorboard/validation_loss_lr{}_nh{}/'.format(lr, nh), sess.graph)
+                train_loss_writer = tf.summary.FileWriter('tensorboard/training_loss_lr{}_nh{}/'.format(lr, nh), sess.graph)
+                val_loss_writer = tf.summary.FileWriter('tensorboard/validation_loss_lr{}_nh{}/'.format(lr, nh), sess.graph)
     
-            for step in range(1, training_steps+1):
-                batch_x = series_mag[(step-1)%32 * batch_size:min(((step-1)%32+1) * batch_size, series_mag.shape[0])]
-                batch_x = batch_x.reshape((batch_x.shape[0], timesteps, num_input), order='F')
-                batch_y = relaxation_times[(step-1)%32 * batch_size:min(((step-1)%32+1) * batch_size, series_mag.shape[0])]
+                for step in range(1, training_steps+1):
+                    batch_x = series_mag[(step-1)%32 * batch_size:min(((step-1)%32+1) * batch_size, series_mag.shape[0])]
+                    batch_x = batch_x.reshape((batch_x.shape[0], timesteps, num_input), order='F')
+                    batch_y = relaxation_times[(step-1)%32 * batch_size:min(((step-1)%32+1) * batch_size, series_mag.shape[0])]
         
         # Training, validation and loss computation
-                training, loss, summary = sess.run([train_op, loss_op, train_loss_summary], feed_dict={X: batch_x, Y: batch_y})
-                train_loss_writer.add_summary(summary, step)
+                    training, loss, summary = sess.run([train_op, loss_op, train_loss_summary], feed_dict={X: batch_x, Y: batch_y})
+                    train_loss_writer.add_summary(summary, step)
         
 #        # Validation
 #        val_loss, val_loss_sum = sess.run([loss_op, val_loss_summary], feed_dict={X:batch_x, Y:batch_y})
 #        val_loss_writer.add_summary(val_loss_sum, step)
-                val_loss, val_summary = sess.run([loss_op, val_loss_summary], feed_dict={X: val_set, Y: relaxation_times[train_size:train_size+val_size]})
-                val_loss_writer.add_summary(val_summary, step)
-        
-            if step % display_step == 0 or step == 1:
-                print("Step " + str(step) + ", Minibatch Loss= " + "{:.10f}".format(val_loss))
-        
-            if step == training_steps:
+                    val_loss, val_summary = sess.run([loss_op, val_loss_summary], feed_dict={X: val_set, Y: relaxation_times[train_size:train_size+val_size]})
+                    val_loss_writer.add_summary(val_summary, step)
+            
+                if step % display_step == 0 or step == 1:
+                    print("Step " + str(step) + ", Minibatch Loss= " + "{:.10f}".format(val_loss))
+            
+                if step == training_steps:
             # Save trained network
-                ckpt_file = ckpt_dir + 'model_lr{}_nh{}_checkpoint{}.ckpt'.format(lr, nh, step)
-                saver.save(sess, ckpt_file)
-    
-            print("Optimization Finished!")
+                    ckpt_file = ckpt_dir + 'model_lr{}_nh{}_checkpoint{}.ckpt'.format(lr, nh, step)
+                    saver.save(sess, ckpt_file)
+        
+                print("Optimization Finished!")
 
 #     Calculate MSE for test time series
 #    times, squared_error_t1, squared_error_t2 = sess.run([out, mse_t1, mse_t2], 
