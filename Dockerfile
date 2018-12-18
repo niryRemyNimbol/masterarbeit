63fa3cca33491a7b57edc9e173f8851311e95735
Niry <niry.andriamanga@telecom-paristech.fr>
2018-12-18 12:02:12 +0100

    New commit after getting back my laptop

--- Dockerfile
+++ Dockerfile
@@ -14,8 +14,9 @@ RUN mkdir /RNN_niry
 
 # Add files to the dockerfile
 COPY rnn_functions.py /RNN_niry/rnn_functions.py
+COPY mrf_recon_rnn_tr_init.py /RNN_niry/mrf_recon_rnn_tr_init.py
 #COPY mrf_recon_rnn_par_search.py /RNN_niry/mrf_recon_rnn_par_search.py
-COPY mrf_recon_rnn_tr_series_len.py /RNN_niry/mrf_recon_rnn_tr_series_len.py
+#COPY mrf_recon_rnn_tr_series_len.py /RNN_niry/mrf_recon_rnn_tr_series_len.py
 #COPY mrf_recon_rnn_fc_init.py /RNN_niry/mrf_recon_rnn_fc_init.py
 #COPY mrf_recon_rnn_fc_series_len.py /RNN_niry/mrf_recon_rnn_fc_series_len.py
 COPY dic.py /RNN_niry/dic.py
@@ -27,6 +28,7 @@ EXPOSE 6006
 
 # Run when the container launches
 
+#CMD exec python mrf_recon_rnn_tr_init.py
 #CMD exec python mrf_recon_rnn_par_search.py
 CMD exec python mrf_recon_rnn_tr_series_len.py
 #CMD exec python mrf_recon_rnn_fc_init.py
--- mrf_recon_rnn_tr_series_len.py
+++ mrf_recon_rnn_tr_series_len.py
@@ -52,15 +52,15 @@ dictionary = dic.dic('recon_q_examples/dict/', 'fisp_mrf', 1000, 10)
 D = dictionary.D[:, dictionary.lut[0, :]>=dictionary.lut[1, :]]
 D /= np.linalg.norm(D, axis=0)
 #dictionary_val = dic.dic('../recon_q_examples/dict/', 'fisp_mrf_val', 1000, 10)
-dictionary_val = dic.dic('recon_q_examples/dict/', 'fisp_mrf_val', 1000, 10)
-D_val = dictionary_val.D[:, dictionary_val.lut[0, :]>=dictionary_val.lut[1, :]]
-D_val /= np.linalg.norm(D_val, axis=0)
+#dictionary_val = dic.dic('recon_q_examples/dict/', 'fisp_mrf_val', 1000, 10)
+#D_val = dictionary_val.D[:, dictionary_val.lut[0, :]>=dictionary_val.lut[1, :]]
+#D_val /= np.linalg.norm(D_val, axis=0)
 permutation = np.random.permutation(D.shape[1])
 
-#train_size = int(np.floor(D.shape[1]*0.8))
-#val_size = D.shape[1]-train_size
-train_size = D.shape[1]
-val_size = D_val.shape[1]
+train_size = int(np.floor(D.shape[1]*0.8))
+val_size = D.shape[1]-train_size
+#train_size = D.shape[1]
+#val_size = D_val.shape[1]
 batches_per_epoch  = int(np.floor(train_size / batch_size))
 
 #series_real = np.real(D.T[permutation])
@@ -69,15 +69,15 @@ batches_per_epoch  = int(np.floor(train_size / batch_size))
 #Ten percent gaussian noise data
 #series_mag = np.abs(D.T[permutation] + 0.01 * np.max(np.real(D)) * np.random.normal(0.0, 1.0, D.T.shape) + 1j * 0.01 * np.max(np.imag(D)) * np.random.normal(0.0, 1.0, D.T.shape))
 series_mag = np.abs(D.T[permutation] + 0.01 * np.max(np.real(D)) * np.random.normal(0.0, 1.0, D.T.shape) + 1j * 0.01 * np.max(np.imag(D)) * np.random.normal(0.0, 1.0, D.T.shape))
-series_mag_val = np.abs(D_val.T + 0.01 * np.max(np.real(D_val)) * np.random.normal(0.0, 1.0, D_val.T.shape) + 1j * 0.01 * np.max(np.imag(D_val)) * np.random.normal(0.0, 1.0, D_val.T.shape))
+#series_mag_val = np.abs(D_val.T + 0.01 * np.max(np.real(D_val)) * np.random.normal(0.0, 1.0, D_val.T.shape) + 1j * 0.01 * np.max(np.imag(D_val)) * np.random.normal(0.0, 1.0, D_val.T.shape))
 #series_phase = np.angle(D.T[permutation])
 #series = np.concatenate([series_mag.T, series_phase.T])
 #series = series.T
 
 train_set = [series_mag[batch_size*step:batch_size*(step+1)].reshape((batch_size, timesteps, num_in_fc), order='F') for step in range(batches_per_epoch)]
 train_set.append(series_mag[batch_size*batches_per_epoch:train_size].reshape((train_size - batch_size*batches_per_epoch, timesteps, num_in_fc), order='F'))
-#val_set = series_mag[train_size:train_size+val_size].reshape((val_size, timesteps, num_in_fc), order='F')
-val_set = series_mag_val.reshape((val_size, timesteps, num_in_fc), order='F')
+val_set = series_mag[train_size:train_size+val_size].reshape((val_size, timesteps, num_in_fc), order='F')
+#val_set = series_mag_val.reshape((val_size, timesteps, num_in_fc), order='F')
 
 #relaxation_times = dictionary.lut[:, dictionary.lut[0, :] >= dictionary.lut[1, :]][0:2].T[permutation]
 relaxation_times = dictionary.lut[:, dictionary.lut[0, :] >= dictionary.lut[1, :]][0:2].T[permutation]
@@ -86,10 +86,10 @@ relaxation_times /= times_max
 
 train_times = [relaxation_times[batch_size*step:batch_size*(step+1)] for step in range(batches_per_epoch)]
 train_times.append(relaxation_times[batch_size*batches_per_epoch:train_size])
-#val_times = relaxation_times[train_size:train_size+val_size]
-val_times = dictionary_val.lut[:, dictionary_val.lut[0, :] >= dictionary_val.lut[1, :]][0:2].T
-val_times_max = np.max(val_times, axis=0)
-val_times /= val_times_max
+val_times = relaxation_times[train_size:train_size+val_size]
+#val_times = dictionary_val.lut[:, dictionary_val.lut[0, :] >= dictionary_val.lut[1, :]][0:2].T
+#val_times_max = np.max(val_times, axis=0)
+#val_times /= val_times_max
 
 from rnn_functions import RNN_with_tr
 
@@ -119,15 +119,15 @@ for timestep in range(1, timesteps+1):
     init = tf.global_variables_initializer()
     
     # Summaries to view in tensorboard
-    #            train_loss_summary = tf.summary.scalar('training_loss', loss_op)
-#    val_loss_summary = tf.summary.scalar('validation_loss', loss_op)
+#    train_loss_summary = [tf.summary.scalar('training_loss_len{}_cell{}'.format(timestep, k), loss_ops[k]) for loss_op in range(len(loss_ops))]
+    val_loss_summary = [tf.summary.scalar('validation_loss_len{}_cell{}'.format(timestep, k), loss_ops[k]) for k in range(len(loss_ops))]
     #merged = tf.summary.merge_all()
     
     # Saver
     saver = tf.train.Saver()
     
     # Restoration directory
-    ckpt_dir = '../rnn_model/'
+#    ckpt_dir = '../rnn_model/'
     
     # Start training
     with tf.Session() as sess:
@@ -135,8 +135,8 @@ for timestep in range(1, timesteps+1):
     # Run the initializer
         sess.run(init)
     
-    #                train_loss_writer = tf.summary.FileWriter('tensorboard/training_loss/', sess.graph)
-#        val_loss_writer = tf.summary.FileWriter('tensorboard/validation_loss_len{}/'.format(num_in_fc*timestep), sess.graph)
+#        train_loss_writer = tf.summary.FileWriter('tensorboard/training_loss/', sess.graph)
+        val_loss_writer = tf.summary.FileWriter('tensorboard/validation_loss/', sess.graph)
         
         counter = 0
         for epoch in range(1, epochs+1):
@@ -150,6 +150,7 @@ for timestep in range(1, timesteps+1):
                 
                 training, batch_loss = sess.run([train_ops, loss_ops], feed_dict={X: batch_x, Y:batch_y})
                 total_loss += batch_loss[-1]
+
     # Training, validation and loss computation
     #                    training, loss, summary = sess.run([train_op, loss_op, train_loss_summary], feed_dict={X: batch_x, Y: batch_y})
     #                    train_loss_writer.add_summary(summary, step)
@@ -160,7 +161,9 @@ for timestep in range(1, timesteps+1):
 #            val_loss, val_summary = sess.run([loss_op, val_loss_summary], feed_dict={X: val_set[:, :timestep, :], Y: val_times})
 #            val_loss_writer.add_summary(val_summary, epoch)
             total_loss /= len(train_set) 
-            val_loss, mse1, mse2 = sess.run([loss_ops, mse_t1, mse_t2], feed_dict={X: val_set[:, :timestep, :], Y: val_times})
+            val_loss, mse1, mse2, summary = sess.run([loss_ops, mse_t1, mse_t2, [val_loss_summary[cell] for cell in range(len(val_loss_summary))]], feed_dict={X: val_set[:, :timestep, :], Y: val_times})
+            for val_sum in summary:
+                val_loss_writer.add_summary(val_sum, epoch)
             val_losses.append(val_loss[-1])
             t1_err[timestep].append(mse1)
             t2_err[timestep].append(mse2)
@@ -191,72 +194,72 @@ for timestep in range(1, timesteps+1):
             elif epoch % early_stop_step == 0:
                 if val_loss[-1] < best_loss:
                     best_loss = val_loss[-1]
-                    counter = 0
-                else:
-                    counter += 1
-            if counter > 20:
-                best_val_losses.append(min(val_losses))
-                break
+    #                counter = 0
+    #            else:
+    #                counter += 1
+    #        if counter > 20:
+    #            best_val_losses.append(min(val_losses))
+    #            break
     
     print("Optimization Finished!")
     print(best_loss)
 
 # plot validation loss as a function of the series length    
-fig1 = plt.figure(1)
-fig1.add_axes([0.2,0.2,0.6,0.6])
-fig1.axes[0].plot(best_val_losses)
-fig1.axes[0].set_xlabel('Series length')
-fig1.axes[0].set_ylabel('Validation loss')
-fig1.axes[0].set_title('Validation loss vs series length', weight='bold')
-fig1.savefig('figures/series_length_tr.pdf')
-
-# plot T1 error evolution with each epoch depending on the rnn length
-fig2, axs2 = plt.subplots(2, 5, sharey=True, figsize=(25,10))
-fig2.text(.5,.99,'T1 error', weight='bold', horizontalalignment='center', verticalalignment='top', size=16 )
-for cell in range(10):
-    for x in range(5):
-        for y in range(2):
-            if y*5+x==cell:
-                axs2[y, x].plot([np.sqrt(t1_err[y*5+x+1][k][cell]) for k in range(len(t1_err[y*5+x+1]))])
-                if cell+1<10:
-                    axs2[y, x].plot([np.sqrt(t1_err[10][k][0]) for k in range(len(t1_err[10]))])
-    axs2[cell//5, cell%5].set_title('Cell {}'.format(cell+1), weight='bold')
-    axs2[cell//5, cell%5].set_xlabel('Epoch')
-    axs2[cell//5, cell%5].set_ylabel('RMSE (s)')
-    axs2[cell//5, cell%5].legend(['{} cell LSTM'.format(n) for n in [cell+1, 10]])
-fig2.savefig('figures/t1_error.pdf')     
-
-# plot T2 error evolution with each epoch depending on the rnn length
-fig3, axs3 = plt.subplots(2, 5, sharey=True, figsize=(25,10))
-fig3.text(.5,.99,'T2 error', weight='bold', horizontalalignment='center', verticalalignment='top', size=16 )
-for cell in range(10):
-    for x in range(5):
-        for y in range(2):
-            if y*5+x==cell:
-                axs3[y, x].plot([np.sqrt(t2_err[y*5+x+1][k][cell]) for k in range(len(t2_err[y*5+x+1]))])
-                if cell+1<10:
-                    axs3[y, x].plot([np.sqrt(t2_err[y*5+x+1][k][0]) for k in range(len(t2_err[10]))])
-    axs3[cell//5, cell%5].set_title('Cell {}'.format(cell+1), weight='bold')
-    axs3[cell//5, cell%5].set_xlabel('Epoch')
-    axs3[cell//5, cell%5].set_ylabel('RMSE (s)')
-    axs3[cell//5, cell%5].legend(['{} cell LSTM'.format(n) for n in [cell+1,10]])
-fig3.savefig('figures/t2_error.pdf')    
-
-# plot each cell error error on the same plot for T1 and T2 (10 cell LSTM)
-fig4, axs4 = plt.subplots(1, 2, sharey=True, figsize=(10, 5))
-fig4.text(.5,.99,'Cell-wise error', weight='bold', horizontalalignment='center', verticalalignment='top', size=16 )
-for cell in range(0,10,3):
-    axs4[0].plot([np.sqrt(t1_err[10][k][cell]) for k in range(len(t1_err[10]))])    
-    axs4[1].plot([np.sqrt(t2_err[10][k][cell]) for k in range(len(t2_err[10]))])
-axs4[0].set_title('T1', weight='bold')
-axs4[1].set_title('T2', weight='bold')
-axs4[0].legend(['Cell {}'.format(n) for n in range(1,11,3)])
-axs4[1].legend(['Cell {}'.format(n) for n in range(1,11,3)])
-axs4[0].set_xlabel('Epoch')
-axs4[0].set_ylabel('RMSE (ms)')
-axs4[1].set_xlabel('Epoch')
-axs4[1].set_ylabel('RMSE (ms)')
-fig4.savefig('figures/cell-wise_error.pdf')
+# fig1 = plt.figure(1)
+# fig1.add_axes([0.2,0.2,0.6,0.6])
+# fig1.axes[0].plot(best_val_losses)
+# fig1.axes[0].set_xlabel('Series length')
+# fig1.axes[0].set_ylabel('Validation loss')
+# fig1.axes[0].set_title('Validation loss vs series length', weight='bold')
+# fig1.savefig('figures/series_length_tr.pdf')
+#
+# # plot T1 error evolution with each epoch depending on the rnn length
+# fig2, axs2 = plt.subplots(2, 5, sharey=True, figsize=(25,10))
+# fig2.text(.5,.99,'T1 error', weight='bold', horizontalalignment='center', verticalalignment='top', size=16 )
+# for cell in range(10):
+#     for x in range(5):
+#         for y in range(2):
+#             if y*5+x==cell:
+#                 axs2[y, x].plot([np.sqrt(t1_err[y*5+x+1][k][cell]) for k in range(len(t1_err[y*5+x+1]))])
+#                 if cell+1<10:
+#                     axs2[y, x].plot([np.sqrt(t1_err[10][k][0]) for k in range(len(t1_err[10]))])
+#     axs2[cell//5, cell%5].set_title('Cell {}'.format(cell+1), weight='bold')
+#     axs2[cell//5, cell%5].set_xlabel('Epoch')
+#     axs2[cell//5, cell%5].set_ylabel('RMSE (s)')
+#     axs2[cell//5, cell%5].legend(['{} cell LSTM'.format(n) for n in [cell+1, 10]])
+# fig2.savefig('figures/t1_error.pdf')
+#
+# # plot T2 error evolution with each epoch depending on the rnn length
+# fig3, axs3 = plt.subplots(2, 5, sharey=True, figsize=(25,10))
+# fig3.text(.5,.99,'T2 error', weight='bold', horizontalalignment='center', verticalalignment='top', size=16 )
+# for cell in range(10):
+#     for x in range(5):
+#         for y in range(2):
+#             if y*5+x==cell:
+#                 axs3[y, x].plot([np.sqrt(t2_err[y*5+x+1][k][cell]) for k in range(len(t2_err[y*5+x+1]))])
+#                 if cell+1<10:
+#                     axs3[y, x].plot([np.sqrt(t2_err[y*5+x+1][k][0]) for k in range(len(t2_err[10]))])
+#     axs3[cell//5, cell%5].set_title('Cell {}'.format(cell+1), weight='bold')
+#     axs3[cell//5, cell%5].set_xlabel('Epoch')
+#     axs3[cell//5, cell%5].set_ylabel('RMSE (s)')
+#     axs3[cell//5, cell%5].legend(['{} cell LSTM'.format(n) for n in [cell+1,10]])
+# fig3.savefig('figures/t2_error.pdf')
+#
+# # plot each cell error error on the same plot for T1 and T2 (10 cell LSTM)
+# fig4, axs4 = plt.subplots(1, 2, sharey=True, figsize=(10, 5))
+# fig4.text(.5,.99,'Cell-wise error', weight='bold', horizontalalignment='center', verticalalignment='top', size=16 )
+# for cell in range(0,10,3):
+#     axs4[0].plot([np.sqrt(t1_err[10][k][cell]) for k in range(len(t1_err[10]))])
+#     axs4[1].plot([np.sqrt(t2_err[10][k][cell]) for k in range(len(t2_err[10]))])
+# axs4[0].set_title('T1', weight='bold')
+# axs4[1].set_title('T2', weight='bold')
+# axs4[0].legend(['Cell {}'.format(n) for n in range(1,11,3)])
+# axs4[1].legend(['Cell {}'.format(n) for n in range(1,11,3)])
+# axs4[0].set_xlabel('Epoch')
+# axs4[0].set_ylabel('RMSE (ms)')
+# axs4[1].set_xlabel('Epoch')
+#axs4[1].set_ylabel('RMSE (ms)')
+#fig4.savefig('figures/cell-wise_error.pdf')
 
 #     Calculate MSE for test time series
 #    times, squared_error_t1, squared_error_t2 = sess.run([out, mse_t1, mse_t2], 
