Correction to the normalisation which was along the wrong dimension

mrf_recon_rnn_fc_init.py
@@ -66,7 +66,10 @@ batches_per_epoch  = int(np.floor(train_size / batch_size))
 #series_imag = np.imag(D.T[permutation])
 #series_mag = np.abs(D.T[permutation])
 #Ten percent gaussian noise data
-series_mag = np.abs(D.T[permutation] + 0.01 * np.max(np.real(D)) * np.random.normal(0.0, 1.0, D.T.shape) + 1j * 0.01 * np.max(np.imag(D)) * np.random.normal(0.0, 1.0, D.T.shape))
+series_mag = np.abs(D.T[permutation] + 0.01 * np.max(np.real(D)) * np.random.normal(0.0, 1.0, D.T.shape) + 1j * 0.01 * np.max(np.imag(D)) * np.random.normal(0.0, 1.0, D.T.shape)).T
+series_mag /= np.linalg.norm(series_mag, axis=0)
+#series_mag /= np.amax(series_mag, axis=0)
+series_mag = series_mag.T
 #series_mag_val = np.abs(D_val.T + 0.01 * np.max(np.real(D_val)) * np.random.normal(0.0, 1.0, D_val.T.shape) + 1j * 0.01 * np.max(np.imag(D_val)) * np.random.normal(0.0, 1.0, D_val.T.shape))
 #series_phase = np.angle(D.T[permutation])
 #series = np.concatenate([series_mag.T, series_phase.T])

mrf_recon_rnn_par_search.py
@@ -17,8 +17,8 @@ import dic
 
 
 # Training Parameters
-epochs = 10000
-learning_rate = [9.0e-1, 1.0e-3]
+epochs = 2000
+learning_rate = [6.0e-2, 4.0e-2]
 display_step = 20
 early_stop_step = 10
 batch_size = 500
@@ -64,7 +64,9 @@ batches_per_epoch  = int(np.floor(train_size / batch_size))
 #series_imag = np.imag(D.T[permutation])
 #series_mag = np.abs(D.T[permutation])
 #Ten percent gaussian noise data
-series_mag = np.abs(D.T[permutation] + 0.01 * np.max(np.real(D)) * np.random.normal(0.0, 1.0, D.T.shape) + 1j * 0.01 * np.max(np.imag(D)) * np.random.normal(0.0, 1.0, D.T.shape))
+series_mag = np.abs(D.T[permutation] + 0.01 * np.max(np.real(D)) * np.random.normal(0.0, 1.0, D.T.shape) + 1j * 0.01 * np.max(np.imag(D)) * np.random.normal(0.0, 1.0, D.T.shape)).T
+series_mag /= np.linalg.norm(series_mag, axis=0)
+series_mag = series_mag.T
 #series_phase = np.angle(D.T[permutation])
 #series = np.concatenate([series_mag.T, series_phase.T])
 #series = series.T

read_mrf.py
@@ -31,8 +31,10 @@ def read_mrf_data(data_path, Nreps, dim):
     
 data_path = '../recon_q_examples/data/data'
 mrf = np.abs(read_mrf_data(data_path, 1000, 256))
-series = mrf.reshape((1000, 256**2))
-series /= np.amax(series, axis=0)
+series = mrf.reshape((1000, 256**2)).T
+series /= np.linalg.norm(series, axis=0)
+#series /= np.amax(series, axis=0)
+series = series.T
 times_max = np.array([4., .6])
 
 # Network Parameters
@@ -74,3 +76,4 @@ for k in range(len(imgs)):
     axs[0, k].imshow(imgs[k][:, :, 0], cmap='hot', origin='lower', vmin=0, vmax=3.0)
     axs[0, k].set_title('T1, timestep {}'.format(k), weight='bold')
     axs[1, k].imshow(imgs[k][:, :, 1], cmap='copper', origin='lower', vmin=0, vmax=0.3)
+fig.show()