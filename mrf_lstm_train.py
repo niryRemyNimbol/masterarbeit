6b52a02dc7158b7504e19eefba567d6a0725265d
Niry <niry.andriamanga@telecom-paristech.fr>
2019-04-05 09:53:26 +0200

    Back to the old learning rate

--- mrf_lstm_train.py
+++ mrf_lstm_train.py
@@ -5,7 +5,7 @@ import numpy as np
 
 epochs = 1000
 batch_size = 500
-learning_rate = 2e-1
+learning_rate = 5e-1
 save_step = 10
 
 timesteps = 10
@@ -15,7 +15,7 @@ num_output = 2
 num_fc = 64
 
 dictionary, relaxation_times = dic.load_dict('recon_q_examples/dict/', 'fisp_mrf_const_tr', 1000, 10)
-train_set, train_times, val_set, val_times, times_max = dic.format_data(dictionary, relaxation_times, timesteps, num_input)
+train_set, train_times, val_set, val_times, times_max = dic.format_data(dictionary, relaxation_times, timesteps, num_input, noise_level=1)
 
 X = tf.placeholder("float", [None, timesteps, num_input])
 Y = tf.placeholder("float", [None, num_output])
